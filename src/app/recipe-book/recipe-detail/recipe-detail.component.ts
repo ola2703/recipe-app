b8d5ad0b8ba815ef52e3d660730e59d411576095
Ola <32633868+ola2703@users.noreply.github.com>
2021-03-21 16:35:21 +0100

    Add app components.

---
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: "recipe-detail",
+    templateUrl: "./recipe-detail.component.html",
+    styleUrls: ["./app.component.css"]
+})
+
+export class RecipeDetailComponent{
+
+}
